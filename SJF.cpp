ede76e62f0385e52e5b4a1ed960951a67dd7ed2c
aggtheprogrammer <124052839+aggtheprogrammer@users.noreply.github.com>
2025-05-19 21:27:15 +0300

    done with SJF

--- SJF.cpp
+++ SJF.cpp
@@ -6,12 +6,44 @@ using namespace std;
 class SFJ: public Scheduler{
     public:
     void schedule(Task* tasks[], int taskCount) override {
-        for(int i=0;i<taskCount;i++){ // Sort tasks based on CPU burst time
+        for(int i=0;i<taskCount;i++){ // sort tasks based on CPU burst time
             int minIdx = i;
             for (int j = i + 1; j < taskCount; ++j) {
                 if (tasks[j]->getCpuBurst() < tasks[minIdx]->getCpuBurst()) // find the task with the minimum CPU burst time
                     minIdx = j;
-        }
+         }
+            if (minIdx != i) { // swaps the tasks
+                Task* tmp   = tasks[i];
+                tasks[i]    = tasks[minIdx]; 
+                tasks[minIdx] = tmp;
+            }
     }
+    /*after sorting, we can calculate waiting time and turnaround time*/ 
+    int currentTime= 0;
+    long long totalWait= 0;
+    long long totalTurnaround= 0;
+
+    cout << "SJF Scheduling Order:\n";
+
+    for (int i = 0; i < taskCount; i++) { // iterate through the sorted tasks
+            Task* t          = tasks[i];
+            int waitTime     = currentTime;
+            int turnTime     = waitTime + t->getCpuBurst();
+
+            cout << "Running Task: "   << t->getName()
+            << " | Burst: "       << t->getCpuBurst()   << " ms"
+            << " | Waiting: "     << waitTime           << " ms"
+            << " | Turnaround: "  << turnTime           << " ms"<<endl;
+
+            currentTime+= t->getCpuBurst(); // updates current time
+            totalWait+= waitTime;
+            totalTurnaround+= turnTime;
+    }
+    /*finds the avgs*/
+    double avgWait = static_cast<double>(totalWait) / taskCount;
+    double avgTurn = static_cast<double>(totalTurnaround) / taskCount;
+
+    cout << "\nAverage Waiting Time   : " << avgWait << " ms"<<endl;
+    cout << "Average Turnaround Time : " << avgTurn << " ms"<<endl;
 }
 };
