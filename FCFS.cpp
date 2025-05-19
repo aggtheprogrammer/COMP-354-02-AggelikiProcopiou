c6baa71343c964711a89007492bdc9ba637b12e1
aggtheprogrammer <124052839+aggtheprogrammer@users.noreply.github.com>
2025-05-19 20:53:52 +0300

    calculations for WT and TT

--- FCFS.cpp
+++ FCFS.cpp
@@ -12,7 +12,7 @@ class FCFS : public Scheduler {
         cout << "FCFS Scheduling:" << endl;
 
         for(int i=0;i<taskCount;i++) {
-            Task* task= tasks[i];
+            Task* task= tasks[i]; // Get the task
 
              int waitingTime = currentTime; 
             int turnaroundTime = waitingTime + task->getCpuBurst();
@@ -22,6 +22,16 @@ class FCFS : public Scheduler {
                  << "ms | Waiting Time: " << waitingTime 
                  << "ms | Turnaround Time: " << turnaroundTime 
                  << "ms"<<endl;
+
+            currentTime += task->getCpuBurst(); // Update current time
+            totalWaitingTime += waitingTime;
+            totalTurnaroundTime += turnaroundTime;
         }
+
+        double avgWaitingTime = double(totalWaitingTime) / taskCount;
+        double avgTurnaroundTime = double(totalTurnaroundTime) / taskCount;
+
+        cout << "\nAverage Waiting Time: " << avgWaitingTime << " ms\n"; // Calculate average waiting time
+        cout << "Average Turnaround Time: " << avgTurnaroundTime << " ms\n"; // Calculate average turnaround time
     }
-}
+};
