c362efaab2a7bf9427446b25623c0e9be0a7e944
aggtheprogrammer <124052839+aggtheprogrammer@users.noreply.github.com>
2025-05-21 21:39:19 +0300

    implemented task loading from file with input validation and whitespace trimming

--- main.cpp
+++ main.cpp
@@ -44,4 +44,40 @@ int main(){
         cerr << "Unknown algorithm: " << algName << '\n';
         return 1;
     }
+
+    ifstream fin(fileName);
+    if (!fin) {
+        cerr << "Cannot open " << fileName << '\n';
+        delete scheduler;
+        return 1;
+    }
+
+    const int MAX_TASKS = 1024;
+    Task* tasks[MAX_TASKS];
+    int   taskCount = 0;
+    string line;
+
+    while (getline(fin, line) && taskCount < MAX_TASKS) {
+        string name;
+        int prio, burst;
+
+       size_t c1 = line.find(',');           // first comma
+       size_t c2 = line.find(',', c1 + 1);   // second comma
+
+       name  = line.substr(0, c1);
+       prio  = stoi(line.substr(c1 + 1, c2 - c1 - 1));
+       burst = stoi(line.substr(c2 + 1));
+
+        while (!name.empty() && (name.front() == ' ' || name.front() == '\t' || name.front() == '\r' || name.front() == '\n')){
+                name.erase(name.begin());
+            }
+
+         while (!name.empty() && (name.back() == ' ' || name.back() == '\t' || name.back() == '\r' || name.back() == '\n')){
+                name.pop_back();
+            }
+
+        tasks[taskCount++] = new Task(name, prio, burst);
+    }
+    assert(taskCount > 0 && "No valid tasks loaded");
+
 }
